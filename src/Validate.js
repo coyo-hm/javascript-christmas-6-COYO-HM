3ea197a3ec6498863d272c5361955ed0ac4c6e82
COYO <dayeon.yoon@gmail.com>
2023-11-14 02:10:25 +0900

    feat: 주문 메뉴를 메뉴와 개수 입력 받기

--- src/Validate.js
+++ src/Validate.js
@@ -1,9 +1,50 @@
+import { MAX_ORDERED_COUNT, MENU } from "./constant.js";
+
 const Validate = {
-  validateDate(date) {
-    if (!/^\d+$/g.test(date)) {
-      throw new Error("[ERROR] 방문 날짜는은 숫자로 입력하셔야 합니다.");
-    } else if (+date < 1 || +date > 31) {
+  validateDate(input) {
+    if (!/^\d+$/g.test(input)) {
+      throw new Error("[ERROR] 방문 날짜는 숫자로 입력하셔야 합니다.");
+    } else if (+input < 1 || +input > 31) {
       throw new Error("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+  },
+
+  isValidCount(count) {
+    return !/^\d+$/g.test(count) || +count < 1;
+  },
+
+  isValidMenu(menu, orderedMenu) {
+    return (
+      MENU.every(({ name }) => name !== menu) || orderedMenu.includes(menu)
+    );
+  },
+
+  isOrderedFood(orderedMenu) {
+    return orderedMenu.every(
+      (menu) => MENU.find(({ name }) => name === menu)?.type === "음료",
+    );
+  },
+
+  validateMenu(menus) {
+    let menuCount = 0;
+    const orderedMenu = [];
+    for (const m of menus) {
+      const [menu, count] = m.split("-");
+      if (this.isValidCount(count) || this.isValidMenu(menu, orderedMenu)) {
+        throw new Error(
+          "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.",
+        );
+      }
+      orderedMenu.push(menu);
+      menuCount += +count;
+      if (menuCount > MAX_ORDERED_COUNT) {
+        throw new Error(
+          "[ERROR] 20개 이상 주문할 수 없습니다. 다시 입력해 주세요.",
+        );
+      }
+    }
+    if (this.isOrderedFood(isOrderedFood)) {
+      throw new Error("[ERROR] 음료만 주문할 수 없습니다. 다시 입력해 주세요.");
     }
   },
 };
