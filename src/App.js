cc6376d6b62fbd18164c54426040b1212c691143
COYO <dayeon.yoon@gmail.com>
2023-11-14 02:39:23 +0900

    feat: 할인 전 총주문 금액 출력

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import InputView from "./InputView.js";
 import OutputView from "./OutputView.js";
+import { MENU } from "./constant.js";
 
 class App {
   async run() {
     const date = await InputView.getDate();
     const menus = await InputView.getMenu();
+    const totalOrderAmount = this.calcTotalOrderAmount(menus);
 
     OutputView.printNotice(date);
     OutputView.printMenu(menus);
+    OutputView.printTotalOrderAmount(totalOrderAmount);
+  }
+
+  calcTotalOrderAmount(menus) {
+    return menus.reduce((total, [menu, count]) => {
+      const price = MENU.find(({ name }) => menu === name)?.price || 0;
+
+      return total + price * count;
+    }, 0);
   }
 }
 
--- src/OutputView.js
+++ src/OutputView.js
@@ -2,14 +2,20 @@ import { Console } from "@woowacourse/mission-utils";
 const OutputView = {
   printNotice(date) {
     Console.print(
-      `12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`,
+      `12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`,
     );
   },
   printMenu(menus) {
     Console.print("<주문 메뉴>");
     menus.forEach((m) => Console.print(`${m[0]} ${m[1]}개`));
+    Console.print(" ");
   },
-  // ...
+
+  printTotalOrderAmount(price) {
+    Console.print("<할인 전 총주문 금액>");
+    Console.print(`${price.toLocaleString("ko-KR")}원`);
+    Console.print(" ");
+  },
 };
 
 export default OutputView;
